d4a0456e57b8fcf6423aed37a97e72f57bb727d8
Kacper Doga <doga.kacper@gmail.com>
2024-05-21 23:48:50 +0200

    layout changes, button panel, reset button

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,46 @@ public class Main extends JFrame {
         setTitle("Virtual World Simulator - s198020");
         setSize(1200, 800);
 
+        confWorld();
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton nextTurnButton = new JButton("Next turn");
+        JButton saveButton = new JButton("Save");
+        JButton loadButton = new JButton("Load");
+        JButton resetButton = new JButton("Reset");
+        JButton skillButton = new JButton("Use skill");
+        buttonPanel.add(nextTurnButton);
+        buttonPanel.add(resetButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(loadButton);
+        buttonPanel.add(skillButton);
+        JTextArea textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        scrollPane.setPreferredSize(new Dimension(400, this.getHeight() - buttonPanel.getHeight() - skillButton.getHeight()));
+
+        JPanel sidePanel = new JPanel();
+        sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
+        sidePanel.add(buttonPanel);
+        sidePanel.add(scrollPane);
+        add(sidePanel, BorderLayout.EAST);
+
+        nextTurnButton.addActionListener(
+                e -> {
+                    world.makeTurn();
+                    for (String message : world.messages) {
+                        textArea.append(message + "\n");
+                    }
+                    world.messages = new ArrayList<>();
+                }
+        );
+
+        resetButton.addActionListener(e -> {confWorld(); textArea.setText("");});
+    }
+
+    void confWorld() {
+        if (world != null)
+            remove(world);
         int width = Integer.parseInt(
                 JOptionPane.showInputDialog(
                         this,
@@ -27,29 +67,12 @@ public class Main extends JFrame {
         human = new Human(width / 2, height / 2, world);
         world.addOrganism(human, width / 2, height / 2);
 
-        var factory = new AnimalFactory(world);
+        var factory = new OrganismFactory(world);
         factory.fillBoardWithOrganisms();
 
         add(world);
-
-        JButton nextTurnButton = new JButton("Next turn");
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-
-        scrollPane.setPreferredSize(new Dimension(400, this.getHeight() - nextTurnButton.getHeight()));
-        add(scrollPane, BorderLayout.EAST);
-
-        nextTurnButton.addActionListener(
-                e -> {
-                    world.makeTurn();
-                    for (String message : world.messages) {
-                        textArea.append(message + "\n");
-                    }
-                    world.messages = new ArrayList<>();
-                }
-        );
-        add(nextTurnButton, BorderLayout.SOUTH);
-
+        revalidate();
+        world.repaint();
     }
 
     public static void main(String[] args) {
